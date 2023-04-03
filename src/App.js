1f1af0f4a5a6280ea351411ea1db78c9325e9cfb
chandanhm1999 <109410990+chandanhm1999@users.noreply.github.com>
2023-04-03 16:52:55 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer/Footer';
+import Header from "./components/header/Header"
+import PlayerPlaylist from './components/playlist/PlayerPlaylist';
+import Sidebar from './components/sidebar/Sidebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className='player-container'>
+        <div className='side-bar'>
+          <Sidebar />
+        </div>
+        <div className='player-playlist'>
+          <PlayerPlaylist />
+        </div>
+      </div>
+      <div className='footer'>
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+
+const header = () => {
+    return (
+        <div className='container'>
+            <img className='navigation-logo' src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_CMYK_Green.png" alt='/' />
+        </div>
+    )
+}
+
+export default header
